c824a4d35b1e0a5b39b5db1e69deb229b25f036b
Lonka-Pardhu <lonkapardhu99@gmail.com>
2024-01-06 19:22:07 +0100

    nav update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,14 +8,52 @@ export default function Navbar() {
                         <img src={appLogo} alt="logo" className='main-logo' />
                     </div>
                     <div className="greet-container">
-                        <p>Hello</p>
-                        <p>Pardhu</p>
+                        <p>MUSIC</p>
+                        <p>NINETY9</p>
                     </div>
                 </div>
-                <span className="material-symbols-outlined home">home</span>
-                <span class="material-symbols-outlined">
-                    explore
-                </span>
+                <div className="nav-list-container">
+                    <div className='nav-list'>
+                        <span className="material-symbols-outlined home">home</span>
+                        <p>Home</p>
+                    </div>
+                    <div className='nav-list'>
+                        <span class="material-symbols-outlined">explore</span>
+                        <p>Explore</p>
+                    </div>
+                    <div className='nav-list'>
+                        <span className='material-symbols-outlined'>trending_up</span>
+                        <p>Trending</p>
+                    </div>
+                    <div className='nav-list'>
+                        <span className='material-symbols-outlined'>podcasts</span>
+                        <p>Podcasts</p>
+                    </div>
+                    <div className='nav-list'>
+                        <span className='material-symbols-outlined'>favorite</span>
+                        <p>Favorites</p>
+                    </div>
+                </div>
+                <div className="nav-user-playlist-container">
+                    <div className="user-playlist-heading">
+                        <p>Your Playlist</p>
+                    </div>
+
+                    <div className="user-playlists-container">
+                        <div className="u-playlist-one u-playlist">
+                            <span class="material-symbols-outlined">sort</span>
+                            <p>user playlist1</p>
+                        </div>
+                        <div className="u-playlist-two u-playlist">
+                            <span class="material-symbols-outlined">sort</span>
+                            <p>user playlist2</p>
+                        </div>
+                        <div className="u-playlist-three u-playlist">
+                            <span class="material-symbols-outlined">sort</span>
+                            <p>user playlist3</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
